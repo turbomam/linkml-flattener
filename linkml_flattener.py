107970af4aa6eefff8505f9eed7764c60733b3be
Mark A. Miller <MAM@lbl.gov>
2025-05-27 12:01:52 -0400

    Initial commit

---
+++ linkml_flattener.py
@@ -0,0 +1,14 @@
+from importlib.resources import files, as_file
+
+from linkml_runtime.utils.schemaview import SchemaView
+
+
+def load_runtime_metamodel() -> SchemaView:
+    resource = files("linkml_runtime.linkml_model.model.schema").joinpath("meta.yaml")
+    with as_file(resource) as path:
+        return SchemaView(path)
+
+
+sv = load_runtime_metamodel()
+
+print(sv.get_class("slot_definition"))
