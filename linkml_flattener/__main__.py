2c4f062bd867f5bfe0de846040a8c347b2196a50
Mark A. Miller <MAM@lbl.gov>
2025-05-27 13:50:01 -0400

    mostly exhaustive slots

--- linkml_flattener/__main__.py
+++ linkml_flattener/__main__.py
@@ -10,7 +10,16 @@ def load_runtime_metamodel() -> SchemaView:
 
 def main():
     sv = load_runtime_metamodel()
-    print(sv.get_class("slot_definition"))
+    # print(sv.get_class("slot_definition"))
+
+    class_name = "slot_definition"
+
+    # Get all slots (including inherited ones)
+    applicable_slots = sv.class_induced_slots(class_name)
+
+    # Print name and range of each
+    for slot in applicable_slots:
+        print(f"{slot.name}: {slot.range}  {slot.multivalued = }")
 
 
 if __name__ == "__main__":
